2a31240eac217dadc50326d1f43aecae4b1c517f
Leandro Milke Cavalheiro <leandromilkecavalheiro@gmail.com>
2022-09-01 11:52:33 -0300

    Continuação do menu

--- ProjetoAluguel/src/Main.java
+++ ProjetoAluguel/src/Main.java
@@ -1,6 +1,7 @@
 import menu.Menu;
 import repository.AdministradorRepository;
 import repository.ClienteRepository;
+import repository.VendedorRepository;
 
 import java.util.Scanner;
 
@@ -9,10 +10,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         AdministradorRepository admRepository = new AdministradorRepository();
-        ClienteRepository 
-
+        ClienteRepository clienteRepository = new ClienteRepository();
+        VendedorRepository vendedorRepository = new VendedorRepository();
+        VendedorRepository veiculoRepository = new VendedorRepository();
 
         boolean continua = true;
+        do {
+            Menu.menu1();
+            int opcao1 = sc.nextInt();
+            switch (opcao1){
+                case 1:
+                    Menu.menuCliente1();
+                    break;
+                case 2:
+                    Menu.menuVendedor1();
+                    break;
+                case 3:
+                    Menu.menuAdministrador1();
+                    break;
+                case 0:
+                    continua = false;
+                    break;
+                default:
+                    System.out.println("Alternatva inválida. Tente novamente !!!");
+                    break;
+            }
+
+        }while (continua);
 
 
     }
