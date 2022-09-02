ba30f7e6c731bacde5e78e54ac5a0b95a040215a
Leandro Milke Cavalheiro <leandromilkecavalheiro@gmail.com>
2022-09-02 10:19:56 -0300

    Revert "Continuação do menu"
    This reverts commit 2a31240eac217dadc50326d1f43aecae4b1c517f.

--- ProjetoAluguel/src/Main.java
+++ ProjetoAluguel/src/Main.java
@@ -2,7 +2,6 @@
 import menu.Menu;
 import repository.AdministradorRepository;
 import repository.ClienteRepository;
-import repository.VendedorRepository;
 
 import java.util.Scanner;
 
@@ -11,33 +10,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         AdministradorRepository admRepository = new AdministradorRepository();
-        ClienteRepository clienteRepository = new ClienteRepository();
-        VendedorRepository vendedorRepository = new VendedorRepository();
-        VendedorRepository veiculoRepository = new VendedorRepository();
+        ClienteRepository 
+
 
         boolean continua = true;
-        do {
-            Menu.menu1();
-            int opcao1 = sc.nextInt();
-            switch (opcao1){
-                case 1:
-                    Menu.menuCliente1();
-                    break;
-                case 2:
-                    Menu.menuVendedor1();
-                    break;
-                case 3:
-                    Menu.menuAdministrador1();
-                    break;
-                case 0:
-                    continua = false;
-                    break;
-                default:
-                    System.out.println("Alternatva inválida. Tente novamente !!!");
-                    break;
-            }
-
-        }while (continua);
 
 
 =======
